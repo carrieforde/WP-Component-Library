a14a1230cab7eb0fe6eb7aa897c2bd7b333f85a2
Carrie Forde <carrie@carrieforde.com>
2017-06-03 23:53:25 -0700

    Update rewrite slug to be plural

--- includes/class-component.php
+++ includes/class-component.php
@@ -55,7 +55,7 @@ class WPCL_Component extends CPT_Core {
 				),
 				'menu_icon' => 'dashicons-archive',
 				'public'    => true,
-				'rewrite'   => array( 'slug' => 'component' ),
+				'rewrite'   => array( 'slug' => 'components' ),
 			)
 		);
 	}
