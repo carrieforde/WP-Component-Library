9f3254de394dea46eb91e774a7ed7ef6c9f95f96
Carrie Forde <carrie@carrieforde.com>
2017-06-03 19:09:36 -0700

    Adds functionality to use template parts :boom:

--- includes/class-component.php
+++ includes/class-component.php
@@ -13,7 +13,7 @@ require_once dirname( __FILE__ ) . '/../vendor/cpt-core/CPT_Core.php';
  *
  * @since 0.0.0
  *
- * @see   https://github.com/WebDevStudios/CPT_Core
+ * @link   https://github.com/WebDevStudios/CPT_Core
  */
 class WPCL_Component extends CPT_Core {
 	/**
@@ -133,6 +133,98 @@ class WPCL_Component extends CPT_Core {
 	}
 
 	/**
+	 * Retrieve the name of the highest priority template file that exists.
+	 *
+	 * Searches in the STYLESHEETPATH before TEMPLATEPATH so that themes which
+	 * inherit from a parent theme can just overload one file. If the template is
+	 * not found in either of those, it looks in the theme-compat folder last.
+	 *
+	 * Modified from example on Pippin's Plugins.
+	 *
+	 * @link  https://pippinsplugins.com/template-file-loaders-plugins/
+	 *
+	 * @since 0.0.0
+	 *
+	 * @param string|array $template_names Template file(s) to search for, in order.
+	 * @param bool $load If true the template file will be loaded if it is found.
+	 * @param bool $require_once Whether to require_once or require. Default true.
+	 *                            Has no effect if $load is false.
+	 * @return string The template filename if one is located.
+	 */
+	public function locate_component_template( $template_names, $load = false, $require_once = true ) {
+
+		// No file found yet
+		$located = false;
+
+		// Try to find a template file
+		foreach ( (array) $template_names as $template_name ) {
+
+			// Continue if template is empty.
+			if ( empty( $template_name ) ) {
+				continue;
+			}
+
+			// Trim off any slashes from the template name
+			$template_name = ltrim( $template_name, '/' );
+
+			// Check child theme first.
+			if ( file_exists( trailingslashit( get_stylesheet_directory() ) . 'template-parts/components/' . $template_name ) ) {
+				$located = trailingslashit( get_stylesheet_directory() ) . 'template-parts/components/' . $template_name;
+				break;
+
+			// Check parent theme next.
+			} elseif ( file_exists( trailingslashit( get_template_directory() ) . 'template-parts/components/' . $template_name ) ) {
+				$located = trailingslashit( get_template_directory() ) . 'template-parts/components/' . $template_name;
+				break;
+
+			// Check theme compatibility last.
+			} elseif ( file_exists( trailingslashit( $this->plugin->path ) . 'component-templates/' . $template_name ) ) {
+				$located = trailingslashit( $this->plugin->path ) . 'component-templates/' . $template_name;
+				break;
+			}
+		}
+
+		if ( ( true === $load ) && ! empty( $located ) ) {
+			load_template( $located, $require_once );
+		}
+
+		return $located;
+	}
+
+	/**
+	 * Retrieves a template part
+	 *
+	 * Modified from example on Pippin's Plugins.
+	 *
+	 * @link  https://pippinsplugins.com/template-file-loaders-plugins/
+	 *
+	 * @since 0.0.0
+	 *
+	 * @param string $slug
+	 * @param string $name Optional. Default null
+	 */
+	public function get_component_template_part( $slug, $name = null, $load = true ) {
+
+		// Execute code for this part
+		do_action( 'get_template_part_' . $slug, $slug, $name );
+
+		// Setup possible parts
+		$templates = array();
+
+		if ( isset( $name ) ) {
+			$templates[] = $slug . '-' . $name . '.php';
+		}
+
+		$templates[] = $slug . '.php';
+
+		// Allow template parts to be filtered
+		$templates = apply_filters( 'get_component_template_part', $templates, $slug, $name );
+
+		// Return the part that is found
+		return $this->locate_component_template( $templates, $load, false );
+	}
+
+	/**
 	 * Cycle through flexible content and display the corresponding markup.
 	 *
 	 * @param  int  $post_id  ID of the post.
@@ -157,6 +249,11 @@ class WPCL_Component extends CPT_Core {
 				case 'image_hero' :
 
 					wp_component_library()->image_hero->image_hero_markup( $post_id, $count );
+					break;
+
+				case 'colors' :
+
+					include( $this->get_component_template_part( 'component', 'colors', false ) );
 					break;
 			}
 		}
