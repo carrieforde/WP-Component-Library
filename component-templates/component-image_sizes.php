58342f56aa371d86ff8e975826f603bd1b862e3e
Carrie Forde <carrie@carrieforde.com>
2017-07-05 13:19:53 -0700

    Adds markup for the image sizes component

---
+++ component-templates/component-image_sizes.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Image Sizes Component
+ *
+ * @since   0.0.0
+ * @package WP_Component_Library
+ */
+?>
+
+<section class="wpcl-component component-image-sizes">
+	<?php wpcl_get_image_sizes(); ?>
+</section>
