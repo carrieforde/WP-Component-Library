36b1cab76186d686fbd5ccf1d9923541ec356f25
Carrie Forde <carrie@carrieforde.com>
2017-07-05 11:01:33 -0700

    Adds escaping for all static strings

--- component-templates/component-form_elements.php
+++ component-templates/component-form_elements.php
@@ -12,54 +12,52 @@
 	<form method="GET" action="">
 		<ul>
 			<li>
-				<label for="text">Text</label>
+				<label for="text"><?php esc_html_e( 'Text', 'wp-component-library' ); ?></label>
 				<input type="text" id="text" placeholder="First Name">
 			</li>
 			<li>
-				<label for="email">Email</label>
+				<label for="email"><?php esc_html_e( 'Email', 'wp-component-library' ); ?></label>
 				<input type="email" id="email" placeholder="example@mail.com" invalid>
 			</li>
 			<li>
-				<label for="email-invalid">Email - Invalid Input</label>
+				<label for="email-invalid"><?php esc_html_e( 'Email - Invalid Input', 'wp-component-library' ); ?></label>
 				<input type="email" id="email-invalid" value="mail">
 			</li>
 			<li>
-				<label for="search">Search</label>
+				<label for="search"><?php esc_html_e( 'Search', 'wp-component-library' ); ?></label>
 				<input type="search" id="search" value="Search">
 			</li>
 			<li>
-				<h2>Radio Buttons</h2>
-
-				<input type="radio" id="true" value="true" name="radios"><label for="true">True</label>
-				<input type="radio" id="false" value="false" name="radios"><label for="false">False</label>
+				<p><?php esc_html_e( 'Radio Buttons', 'wp-component-library' ); ?></p>
+				<input type="radio" id="true" value="true" name="radios"><label for="true"><?php esc_html_e( 'True', 'wp-component-library' ); ?></label>
+				<input type="radio" id="false" value="false" name="radios"><label for="false"><?php esc_html_e( 'False', 'wp-component-library' ); ?></label>
 			</li>
 			<li>
-				<h2>Checkboxes</h2>
-
-				<input type="checkbox" id="pizza" value="pizza"><label for="pizza" name="foods">Pizza</label>
-				<input type="checkbox" id="burritos" value="burritos" name="foods"><label for="burritos">Burritos</label>
-				<input type="checkbox" id="burgers" value="burgers" name="foods"><label for="burgers">Burgers</label>
+				<p><?php esc_html_e( 'Checkboxes', 'wp-component-library' ); ?></p>
+				<input type="checkbox" id="pizza" value="pizza"><label for="pizza" name="foods"><?php esc_html_e( 'Pizza', 'wp-component-library' ); ?></label>
+				<input type="checkbox" id="burritos" value="burritos" name="foods"><label for="burritos"><?php esc_html_e( 'Burritos', 'wp-component-library' ); ?></label>
+				<input type="checkbox" id="burgers" value="burgers" name="foods"><label for="burgers"><?php esc_html_e( 'Burgers', 'wp-component-library' ); ?></label>
 			</li>
 			<li>
-				<label for="select">Select</label>
+				<label for="select"><?php esc_html_e( 'Select', 'wp-component-library' ); ?></label>
 				<select>
-					<option value="" disabled selected>Month</option>
-					<option value="January">January</option>
-					<option value="February">February</option>
-					<option value="March">March</option>
-					<option value="April">April</option>
-					<option value="May">May</option>
-					<option value="June">June</option>
-					<option value="July">July</option>
-					<option value="August">August</option>
-					<option value="September">September</option>
-					<option value="October">October</option>
-					<option value="November">November</option>
-					<option value="December">December</option>
+					<option value="" disabled selected><?php esc_html_e( 'Month', 'wp-component-library' ); ?></option>
+					<option value="January"><?php esc_html_e( 'January', 'wp-component-library' ); ?></option>
+					<option value="February"><?php esc_html_e( 'February', 'wp-component-library' ); ?></option>
+					<option value="March"><?php esc_html_e( 'March', 'wp-component-library' ); ?></option>
+					<option value="April"><?php esc_html_e( 'April', 'wp-component-library' ); ?></option>
+					<option value="May"><?php esc_html_e( 'May', 'wp-component-library' ); ?></option>
+					<option value="June"><?php esc_html_e( 'June', 'wp-component-library' ); ?></option>
+					<option value="July"><?php esc_html_e( 'July', 'wp-component-library' ); ?></option>
+					<option value="August"><?php esc_html_e( 'August', 'wp-component-library' ); ?></option>
+					<option value="September"><?php esc_html_e( 'September', 'wp-component-library' ); ?></option>
+					<option value="October"><?php esc_html_e( 'October', 'wp-component-library' ); ?></option>
+					<option value="November"><?php esc_html_e( 'November', 'wp-component-library' ); ?></option>
+					<option value="December"><?php esc_html_e( 'December', 'wp-component-library' ); ?></option>
 				</select>
 			</li>
 			<li>
-				<label for="textarea">Textarea</label>
+				<label for="textarea"><?php esc_html_e( 'Textarea', 'wp-component-library' ); ?></label>
 				<textarea></textarea>
 			</li>
 		</ul>
